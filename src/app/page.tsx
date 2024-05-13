6e11d7003a8dd671db529765dd0521a55d8908dd
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2024-05-13 20:15:23 +0300

    Update
    Implemented SASS.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from './page.module.css'
+import '../styles/styles.scss';
 
 const inter = Inter({ subsets: ['latin'] })
 
