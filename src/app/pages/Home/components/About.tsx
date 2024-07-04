9de0d0b2d1c9e79f8e6fdae7a679f0385dbf3ccd
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2024-07-04 23:47:55 +0300

    Update
    Updated for HomePage.

--- src/app/pages/Home/components/About.tsx
+++ src/app/pages/Home/components/About.tsx
@@ -1,9 +1,19 @@
-
 const About = () => {
   return (
     <div id="headline">
       <div className="container mx-auto px-4">
-        <p>About</p>
+        <span>Hi, I'm Mehmet.</span>
+        <p>
+          I have over 6 years of experience as a Frontend Developer, working
+          across sectors including E-commerce, Localization, Technology,
+          Advertising, and Insurance. My focus is on creating user-centric
+          solutions, and I am dedicated to continuous learning and development.
+          My objective is to deliver optimal user experiences for every project
+          and provide added value to our partners. I look forward to
+          contributing my experience to future projects.
+        </p>
+        <button className="bg-blue-500 text-white px-4 py-2 rounded-lg shadow hover:bg-blue-600">About Me</button>
+        <button className="bg-blue-500 text-white px-4 py-2 rounded-lg shadow hover:bg-blue-600">Download CV</button>
       </div>
     </div>
   );
--- src/app/pages/Home/components/Contact.tsx
+++ src/app/pages/Home/components/Contact.tsx
@@ -1,9 +1,44 @@
 
+import { FaLinkedin, FaGithub, FaTwitter, FaEnvelope } from "react-icons/fa";
+
 const Contact = () => {
     return (
       <div id="headline">
         <div className="container mx-auto px-4">
-          <p>Contact</p>
+        <div className="flex items-center space-x-4">
+              <a
+                href="https://www.linkedin.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-gray-700 hover:text-blue-600"
+              >
+                <FaLinkedin className="w-6 h-6" />
+              </a>
+              <a
+                href="https://github.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-gray-700 hover:text-gray-800"
+              >
+                <FaGithub className="w-6 h-6" />
+              </a>
+              <a
+                href="https://twitter.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-gray-700 hover:text-blue-400"
+              >
+                <FaTwitter className="w-6 h-6" />
+              </a>
+              <a
+                href="mailto:#"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-gray-700 hover:text-blue-400"
+              >
+                <FaEnvelope className="w-6 h-6" />
+              </a>
+            </div>
         </div>
       </div>
     );
