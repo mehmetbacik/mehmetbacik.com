0fbe0817182094950aae90422fa173edb292bc58
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2024-07-06 00:59:38 +0300

    Update
    Updated for ExperienceComponent.

--- src/app/experience/components/Experience.tsx
+++ src/app/experience/components/Experience.tsx
@@ -1,4 +1,5 @@
 import { FaBriefcase } from "react-icons/fa";
+import Image from "next/image";
 
 const Experience = () => {
   return (
@@ -8,14 +9,19 @@ const Experience = () => {
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           <div className="bg-white rounded-lg shadow-lg p-6 flex items-center">
             <div className="mr-4">
-              <FaBriefcase className="text-4xl text-blue-500" />
+              <Image
+                src="/library/images/companies/ekspar.jpg"
+                alt="EksparLogo"
+                width={50}
+                height={50}
+              />
             </div>
             <div>
               <h3 className="text-xl font-semibold text-gray-800 mb-2">
-                Job Title
+                Frontend Team Lead
               </h3>
-              <p className="text-gray-600 mb-2">Company Name</p>
-              <p className="text-gray-700 mb-4">Date Range</p>
+              <p className="text-gray-600 mb-2">Ekspar</p>
+              <p className="text-gray-700 mb-4">Nov 2023 - Jul 2024</p>
               <p className="text-gray-700">
                 Description of responsibilities and achievements in this
                 position.
@@ -25,14 +31,19 @@ const Experience = () => {
 
           <div className="bg-white rounded-lg shadow-lg p-6 flex items-center">
             <div className="mr-4">
-              <FaBriefcase className="text-4xl text-blue-500" />
+              <Image
+                src="/library/images/companies/hogarth.jpg"
+                alt="HogarthLogo"
+                width={50}
+                height={50}
+              />
             </div>
             <div>
               <h3 className="text-xl font-semibold text-gray-800 mb-2">
-                Job Title
+                Frontend Developer
               </h3>
-              <p className="text-gray-600 mb-2">Company Name</p>
-              <p className="text-gray-700 mb-4">Date Range</p>
+              <p className="text-gray-600 mb-2">Hogarth</p>
+              <p className="text-gray-700 mb-4">Aug 2022 - Oct 2023</p>
               <p className="text-gray-700">
                 Description of responsibilities and achievements in this
                 position.
@@ -42,14 +53,41 @@ const Experience = () => {
 
           <div className="bg-white rounded-lg shadow-lg p-6 flex items-center">
             <div className="mr-4">
-              <FaBriefcase className="text-4xl text-blue-500" />
+              <Image
+                src="/library/images/companies/tsoft.jpg"
+                alt="TsoftLogo"
+                width={50}
+                height={50}
+              />
             </div>
             <div>
               <h3 className="text-xl font-semibold text-gray-800 mb-2">
-                Job Title
+                Frontend Developer
               </h3>
-              <p className="text-gray-600 mb-2">Company Name</p>
-              <p className="text-gray-700 mb-4">Date Range</p>
+              <p className="text-gray-600 mb-2">T-Soft</p>
+              <p className="text-gray-700 mb-4">May 2018 - Aug 2022</p>
+              <p className="text-gray-700">
+                Description of responsibilities and achievements in this
+                position.
+              </p>
+            </div>
+          </div>
+
+          <div className="bg-white rounded-lg shadow-lg p-6 flex items-center">
+            <div className="mr-4">
+              <Image
+                src="/library/images/companies/model.jpg"
+                alt="ModelLogo"
+                width={50}
+                height={50}
+              />
+            </div>
+            <div>
+              <h3 className="text-xl font-semibold text-gray-800 mb-2">
+                Software Developer
+              </h3>
+              <p className="text-gray-600 mb-2">Model</p>
+              <p className="text-gray-700 mb-4">Jun 2015 - Aug 2015</p>
               <p className="text-gray-700">
                 Description of responsibilities and achievements in this
                 position.
