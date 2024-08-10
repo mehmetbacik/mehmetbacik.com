38b9685301bde7280173fad7554c6e6b775581df
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2024-08-10 20:36:17 +0300

    Update
    Updated for project files.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,12 @@
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import '../styles/styles.scss';
-import Home from './home/page';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Page() {
   return (
     <div>
-      <Home />
+      Test
     </div>
   )
 }
