04fef677dcebc39d3d95c77e4846c0f6c308da6e
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2024-08-19 20:36:10 +0300

    Update
    Updated for Technology component.

--- src/app/technology/Technology.tsx
+++ src/app/technology/Technology.tsx
@@ -2,8 +2,6 @@
 
 import React from "react";
 import { Icon } from "@iconify/react";
-import { Tooltip } from "react-tooltip";
-import "react-tooltip/dist/react-tooltip.css";
 import styles from "./styles/Technology.module.scss";
 import { technologyData } from "@/data/technologyData";
 
@@ -18,6 +16,39 @@ const Technology: React.FC<TechnologyProps> = ({ id }) => {
         <div className={styles.technology_headline}>
           <h1 className={styles.technology_bigtitle}>TechStack</h1>
           <h2 className={styles.technology_title}>Tech Stack</h2>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-8 auto-rows-fr">
+          {Object.entries(technologyData).map(([group, techList], index) => (
+            <div
+              key={index}
+              className="p-4 bg-gray-100 rounded-lg shadow-lg flex flex-col h-full"
+            >
+              <h3 className="text-lg font-semibold text-center mb-4">
+                {group
+                  .split("_")
+                  .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+                  .join(" ")}
+              </h3>
+              <div className="grid grid-cols-4 gap-4 flex-grow">
+                {techList.map((tech, techIndex) => (
+                  <div
+                    key={techIndex}
+                    className="flex flex-col items-center justify-center"
+                  >
+                    <Icon
+                      icon={tech.icon}
+                      width={40}
+                      height={40}
+                      className="text-blue-700 mb-2"
+                    />
+                    <span className="text-sm text-gray-700 text-center">
+                      {tech.name}
+                    </span>
+                  </div>
+                ))}
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </section>
--- src/data/technologyData.ts
+++ src/data/technologyData.ts
@@ -1,6 +1,70 @@
-import { TechnologyItem } from "../types/types";
-
-export const technologyData: TechnologyItem[]  = [
-  { name: "React", icon: "logos:react" },
-  { name: "Vue", icon: "logos:vue" },
-];
+export const technologyData = {
+  frontend: [
+    { name: "React.js", icon: "logos:react" },
+    { name: "Vue.js", icon: "logos:vue" },
+    { name: "Angular", icon: "logos:angular-icon" },
+    { name: "Next.js", icon: "logos:nextjs-icon" },
+    { name: "Typescript", icon: "logos:typescript-icon" },
+    { name: "Vite", icon: "logos:vitejs" },
+    { name: "React Router", icon: "logos:react-router" },
+    { name: "Vue Router", icon: "logos:vue" },
+    { name: "Redux", icon: "logos:redux" },
+    { name: "Vuex", icon: "logos:vue" },
+    { name: "Swr", icon: "logos:swr" },
+    { name: "Sass", icon: "logos:sass" },
+    { name: "Less", icon: "logos:less" },
+    { name: "Bootstrap", icon: "logos:bootstrap" },
+    { name: "TailwindCss", icon: "logos:tailwindcss-icon" },
+    { name: "HTML", icon: "logos:html-5" },
+    { name: "CSS", icon: "logos:css-3" },
+    { name: "Javascript", icon: "logos:javascript" },
+    { name: "Jquery", icon: "logos:jquery" },
+  ],
+  backend: [
+    { name: "Node.js", icon: "logos:nodejs-icon" },
+    { name: "Python", icon: "logos:python" },
+    { name: "PHP", icon: "logos:php" },
+    { name: "C", icon: "logos:c" },
+    { name: "MySQL", icon: "logos:mysql" },
+    { name: "Swagger", icon: "logos:swagger" },
+  ],
+  development_tools: [
+    { name: "Docker", icon: "logos:docker-icon" },
+    { name: "Git", icon: "logos:git-icon" },
+    { name: "Azure Devops", icon: "cib:azure-devops" },
+    { name: "SVN", icon: "logos:subversion" },
+    { name: "npm", icon: "logos:npm-icon" },
+    { name: "Yarn", icon: "logos:yarn" },
+  ],
+  design: [
+    { name: "Figma", icon: "logos:figma" },
+    { name: "Sketch", icon: "logos:sketch" },
+    { name: "Adobe Xd", icon: "logos:adobe-xd" },
+    { name: "Zeplin", icon: "logos:zeplin" },
+    { name: "Photoshop", icon: "logos:adobe-photoshop" },
+    { name: "Premiere Pro", icon: "logos:adobe-premiere" },
+  ],
+  testing: [
+    { name: "Jest", icon: "logos:jest" },
+    { name: "Selenium", icon: "logos:selenium" },
+  ],
+  editors: [
+    { name: "VS Code", icon: "logos:visual-studio-code" },
+    { name: "Xcode", icon: "logos:xcode" },
+    { name: "SublimeText", icon: "logos:sublimetext-icon" },
+    { name: "NotePad++", icon: "simple-icons:notepadplusplus" },
+    { name: "BBEdit", icon: "logos:apple-app-store" },
+    { name: "Dreamweaver", icon: "logos:adobe-dreamweaver" },
+  ],
+  project_management: [
+    { name: "Trello", icon: "logos:trello" },
+    { name: "Radar", icon: "logos:apple-app-store" },
+    { name: "Jira", icon: "logos:jira" },
+  ],
+  cms: [
+    { name: "Wordpress", icon: "logos:wordpress" },
+    { name: "Opencart", icon: "logos:opencart" },
+    { name: "Blogger", icon: "logos:blogger" },
+  ],
+  version_control: [{ name: "Github", icon: "logos:github-icon" }],
+};
