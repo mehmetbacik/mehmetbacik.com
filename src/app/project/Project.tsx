543df12cead65e91ac14e8a030de0de24bca90b5
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2024-08-27 15:29:47 +0300

    Update
    Updated components for responsive.

--- src/app/project/Project.tsx
+++ src/app/project/Project.tsx
@@ -145,7 +145,7 @@ const Project: React.FC<ProjectProps> = ({ id }) => {
             </Swiper>
           </div>
           <div
-            className={`grid-cols-2 sm:grid-cols-2 lg:grid-cols-4 ${styles.project_card}`}
+            className={`grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 ${styles.project_card}`}
           >
             {paginatedProjects.map((project) => (
               <a
