a4f310ebf6264687a22e47b65df91d8add7ac415
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2024-08-27 21:21:02 +0300

    Update
    Updated for project error.

--- src/app/technology/Technology.tsx
+++ src/app/technology/Technology.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import { Icon } from "@iconify/react";
 import styles from "./styles/Technology.module.scss";
 import { technologyData } from "@/data/technologyData";
@@ -10,6 +10,12 @@ interface TechnologyProps {
 }
 
 const Technology: React.FC<TechnologyProps> = ({ id }) => {
+  const [openCardIndex, setOpenCardIndex] = useState<number | null>(null);
+
+  const handleToggle = (index: number) => {
+    setOpenCardIndex((prevIndex) => (prevIndex === index ? null : index));
+  };
+
   return (
     <section id={id} className={`container mx-auto px-4 ${styles.technology}`}>
       <div className={styles.technology_content}>
@@ -17,16 +23,25 @@ const Technology: React.FC<TechnologyProps> = ({ id }) => {
           <h1 className={styles.technology_bigtitle}>TechStack</h1>
           <h2 className={styles.technology_title}>Tech Stack</h2>
         </div>
-        <div className={`grid-cols-1 md:grid-cols-2 lg:grid-cols-3 ${styles.technology_list}`}>
+        <div
+          className={`grid-cols-1 md:grid-cols-2 lg:grid-cols-3 ${styles.technology_list}`}
+        >
           {Object.entries(technologyData).map(([group, techList], index) => (
             <div key={index} className={styles.technology_card}>
-              <h3 className={styles.technology_card_title}>
+              <h3
+                className={styles.technology_card_title}
+                onClick={() => handleToggle(index)}
+              >
                 {group
                   .split("_")
                   .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
                   .join(" ")}
               </h3>
-              <div className={styles.technology_card_icons}>
+              <div
+                className={`${styles.technology_card_icons} ${
+                  openCardIndex === index ? styles.open : styles.closed
+                }`}
+              >
                 {techList.map((tech, techIndex) => (
                   <div
                     key={techIndex}
@@ -38,9 +53,7 @@ const Technology: React.FC<TechnologyProps> = ({ id }) => {
                       height={40}
                       className="text-blue-700"
                     />
-                    <span>
-                      {tech.name}
-                    </span>
+                    <span>{tech.name}</span>
                   </div>
                 ))}
               </div>
