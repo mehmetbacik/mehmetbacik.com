d9c7bd77a585885fcbd341cd49a97c902dd236af
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2024-08-30 16:28:05 +0300

    Update
    Updated for description.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata = {
   title: "Mehmet BACIK - Frontend Developer",
-  description: "Personal portfolio website",
+  description:
+    "Mehmet Bacık, an experienced Frontend Developer. Visit his personal portfolio website to explore his projects, technical skills, and insights into his software development journey.",
 };
 
 export default function RootLayout({
